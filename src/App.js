cc34001e5213613d10d04ef4272146ae3db98d68
abhichukka08 <159679865+abhishekchukka@users.noreply.github.com>
2024-08-21 15:03:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ export default function App() {
 
         try {
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${key}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${key}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) {
